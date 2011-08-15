3a7bde198fdf5db130ab5991a77aba4ead5673b5
unknown <JuanFran@.(none)>
2011-08-15 16:44:02 +0100

    Initial commit

---
+++ SocialDemocracy/modules/MainModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace SocialDemocracy.modules
+{
+    public class MainModule:NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return "<h1>Welcome to Social Democracy!</h1><p>Nothing to see here at the moment.</p>";
+            };
+        }
+    }
+}
